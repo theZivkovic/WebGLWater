119f7031881cd58d13eb65041bdca6c60f71728e
Dejan <dejan.zivkovic90@gmail.com>
2017-06-26 13:25:42 +0200

    Fix for spreading freeze bug

--- assets/shaders/waterFSH.glsl
+++ assets/shaders/waterFSH.glsl
@@ -18,8 +18,8 @@ void main(void)
 {
 	vec2 waterCoords = (waterCoordinates.xy / waterCoordinates.w) / 2.0 + 0.5;
 
-	vec2 waterMovementXCoords = clamp(vec2(waterCoords.x + waterMoveFactor, waterCoords.y), vec2(0.0, 0.0), vec2(1.0, 1.0));
-	vec2 waterMovementXYCoords = clamp(vec2(waterCoords.x + waterMoveFactor, waterCoords.y + waterMoveFactor), vec2(0.0, 0.0), vec2(1.0, 1.0));
+	vec2 waterMovementXCoords = vec2(waterCoords.x + waterMoveFactor, waterCoords.y);
+	vec2 waterMovementXYCoords = vec2(waterCoords.x + waterMoveFactor, waterCoords.y + waterMoveFactor);
 	
 	vec2 waterHorizontalDistortion = (texture2D(waterDUDVMap, waterMovementXCoords).rg * 2.0 - 1.0) * waveStrength;
 	vec2 waterDiagonalDistortion = (texture2D(waterDUDVMap, waterMovementXYCoords).rg * 2.0 - 1.0) * waveStrength;
